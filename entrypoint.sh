325d5f14a19932bf3cc4828af5101cd94e08b3e6
Matt Wynne <matt@cucumber.io>
2021-08-16 23:01:07 -0700

    Handle tag prefix / changelog directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-next_version_heading=$(changelog latest)
+next_version_heading=$(changelog latest --filename $2/CHANGELOG.md)
 next_version=${next_version_heading/v}
+next_version=$([[ "$next_version" == There* ]] && echo "" || echo $next_version)
 echo "::set-output name=changelog-latest-version::$next_version"
 
 git fetch --tags
-current_version_label=$(git describe --tags $(git rev-list --tags --max-count=1))
-current_version=${current_version_label/v/}
+current_version_label=$(git describe --tags --match "$1*" --abbrev=0)
+current_version=${current_version_label/$1v/}
 echo "::set-output name=released-version::$current_version"
 
 is_release_required=$([ $next_version == $current_version ] && echo "" || echo "true" )
